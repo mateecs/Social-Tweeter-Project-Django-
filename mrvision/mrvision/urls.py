141911b5de43cd064a993ef2c472555e5915f270
Matee Ur Rehman <mateerehman42@gmail.com>
2025-10-21 01:32:45 +0500

    Finished backend Commit

--- mrvision/mrvision/urls.py
+++ mrvision/mrvision/urls.py
@@ -25,5 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/',include ('posts.urls')),
     path('accounts/',include ('django.contrib.auth.urls')),
+     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    
 
 ] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+ 
